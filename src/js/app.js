7cb3439ab8a9a17e373fdcdb3f8f33f38bbabfa3
Alexander Sabanov <alexsab@mail.ru>
2024-08-21 17:54:17 +0400

    upd re, astro site

--- src/js/app.js
+++ src/js/app.js
@@ -45,7 +45,7 @@ if(lazys.length){
 
 function executeRecaptcha() {
 grecaptcha.ready(function() {
-	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'open'}).then(function(token) {
+	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'auto_team'}).then(function(token) {
 		let formData = new FormData();
 		formData.append('g-recaptcha-response', token);
 		const params = new URLSearchParams([...formData]);
