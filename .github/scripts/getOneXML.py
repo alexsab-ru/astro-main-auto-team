2ac5b35c2bc02efa7df3463f18de74aca7d4f905
Alexander Sabanov <alexsab@mail.ru>
2024-08-28 18:02:01 +0400

    Update getOneXML.py

--- .github/scripts/getOneXML.py
+++ .github/scripts/getOneXML.py
@@ -60,7 +60,7 @@ def main():
 
     # env_xml_url = os.getenv('ENV_XML_URL', '')
     env_xml_url = os.environ['ENV_XML_URL']
-    urls = env_xml_url.strip().split('\n')
+    urls = env_xml_url.strip().split('\r\n')
     print(urls)
 
     if not urls:
