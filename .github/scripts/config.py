e9b02daf628ba51e4e3408e8e80b45a7d6a6a732
Alexander Sabanov <alexsab@mail.ru>
2024-08-28 18:32:34 +0400

    add GAC images

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -121,5 +121,71 @@ model_mapping = {
             "White": "white.webp",
             "Белый": "white.webp",
         }},
+    "GN8": {
+        "folder": "gn8",
+        "color": {
+            "Черный": "black.webp",
+            "Чёрный": "black.webp",
+            "Синий": "blue.webp",
+            "Коричневый": "brown.webp",
+            "Белый": "white.webp",
+        }},
+    "GS3": {
+        "folder": "gs3",
+        "color": {
+            "Синий": "blue.webp",
+            "Серый": "gray.webp",
+            "Графитовый": "grey-graphite.webp",
+            "Графеновый серый": "grey-graphite.webp",
+            "Серебристый": "silver.webp",
+            "Серебряный": "silver.webp",
+            "Белый": "white.webp",
+        }},
+    "GS5": {
+        "folder": "gs5",
+        "color": {
+            "Черный": "black.webp",
+            "Чёрный": "black.webp",
+            "Коричневый": "brown.webp",
+            "Серый": "gray.webp",
+            "Голубой": "ice-blue.webp",
+            "Серебристый": "silver.webp",
+            "Серебряный": "silver.webp",
+            "Белый": "white.webp",
+        }},
+    "GS8, I поколение": {
+        "folder": "gs8",
+        "color": {
+            "Черный": "black.webp",
+            "Чёрный": "black.webp",
+            "Синий": "blue.webp",
+            "Коричневый": "brown.webp",
+            "Серый": "gray.webp",
+            "Голубой": "ice-blue.webp",
+            "Белый": "white.webp",
+        }},
+    "GS8": {
+        "folder": "gs8ii",
+        "color": {
+            "Черный": "black.webp",
+            "Чёрный": "black.webp",
+            "Серый": "gray.webp",
+            "Зеленый": "green.webp",
+            "Зелёный": "green.webp",
+            "Серебристый": "silver.webp",
+            "Серебряный": "silver.webp",
+            "Белый": "white.webp",
+        }},
+    "M8": {
+        "folder": "m8",
+        "color": {
+            "Черный": "black.webp",
+            "Чёрный": "black.webp",
+            "Синий": "blue.webp",
+            "Зеленый": "green.webp",
+            "Зелёный": "green.webp",
+            "Золотой": "gold.webp",
+            "Белый": "white.webp",
+        }},
     # ... добавьте другие модели по мере необходимости
 }
